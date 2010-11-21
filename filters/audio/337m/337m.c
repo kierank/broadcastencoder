fadfdec98e37243c039b84cac16e37f20d8862d2
Kieran Kunhya <kieran@kunhya.com>
2010-11-21 03:19:36 +0000

    Remove x264 licensing notice.

--- filters/audio/337m/337m.c
+++ filters/audio/337m/337m.c
@@ -19,8 +19,6 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02111, USA.
  *
- * This program is also available under a commercial proprietary license.
- * For more information, contact us at licensing@x264.com.
  *****************************************************************************/
 
 #define 337M_SYNCWORD_1_16_BIT 0xf872
