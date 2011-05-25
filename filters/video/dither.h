8f7f6174ac9582bfa95186afb7867027e79070e3
Kieran Kunhya <kieran@kunhya.com>
2011-05-25 16:00:11 +0100

    Disable x264cli's dither and port swscale's dither. Also work with frames in native YUV422P10.

---
+++ filters/video/dither.h
@@ -0,0 +1,56 @@
+/*****************************************************************************
+ * dither.h : OBE video dithering tables
+ *****************************************************************************
+ * Copyright (C) 2010 FFmpeg project
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02111, USA.
+ *
+ *****************************************************************************/
+
+#ifndef OBE_FILTERS_DITHER_H
+#define OBE_FILTERS_DITHER_H
+
+DECLARE_ALIGNED(8, const uint8_t, obe_dithers)[8][8] =
+{
+    { 1,  2,  1,  2,  1,  2,  1,  2 },
+    { 3,  0,  3,  0,  3,  0,  3,  0 },
+    { 1,  2,  1,  2,  1,  2,  1,  2 },
+    { 3,  0,  3,  0,  3,  0,  3,  0 },
+    { 1,  2,  1,  2,  1,  2,  1,  2 },
+    { 3,  0,  3,  0,  3,  0,  3,  0 },
+    { 1,  2,  1,  2,  1,  2,  1,  2 },
+    { 3,  0,  3,  0,  3,  0,  3,  0 },
+};
+
+uint16_t obe_dither_scale[15][16] =
+{
+{    2,    3,    3,    5,    5,    5,    5,    5,    5,    5,    5,    5,    5,    5,    5,    5,},
+{    2,    3,    7,    7,   13,   13,   25,   25,   25,   25,   25,   25,   25,   25,   25,   25,},
+{    3,    3,    4,   15,   15,   29,   57,   57,   57,  113,  113,  113,  113,  113,  113,  113,},
+{    3,    4,    4,    5,   31,   31,   61,  121,  241,  241,  241,  241,  481,  481,  481,  481,},
+{    3,    4,    5,    5,    6,   63,   63,  125,  249,  497,  993,  993,  993,  993,  993, 1985,},
+{    3,    5,    6,    6,    6,    7,  127,  127,  253,  505, 1009, 2017, 4033, 4033, 4033, 4033,},
+{    3,    5,    6,    7,    7,    7,    8,  255,  255,  509, 1017, 2033, 4065, 8129,16257,16257,},
+{    3,    5,    6,    8,    8,    8,    8,    9,  511,  511, 1021, 2041, 4081, 8161,16321,32641,},
+{    3,    5,    7,    8,    9,    9,    9,    9,   10, 1023, 1023, 2045, 4089, 8177,16353,32705,},
+{    3,    5,    7,    8,   10,   10,   10,   10,   10,   11, 2047, 2047, 4093, 8185,16369,32737,},
+{    3,    5,    7,    8,   10,   11,   11,   11,   11,   11,   12, 4095, 4095, 8189,16377,32753,},
+{    3,    5,    7,    9,   10,   12,   12,   12,   12,   12,   12,   13, 8191, 8191,16381,32761,},
+{    3,    5,    7,    9,   10,   12,   13,   13,   13,   13,   13,   13,   14,16383,16383,32765,},
+{    3,    5,    7,    9,   10,   12,   14,   14,   14,   14,   14,   14,   14,   15,32767,32767,},
+{    3,    5,    7,    9,   11,   12,   14,   15,   15,   15,   15,   15,   15,   15,   16,65535,},
+};
+
+#endif
--- filters/video/video.c
+++ filters/video/video.c
@@ -23,10 +23,11 @@
 
 #include <libavutil/cpu.h>
 #include <libswscale/swscale.h>
-#include "x86/vfilter.h"
 #include "common/common.h"
 #include "video.h"
 #include "cc.h"
+#include "dither.h"
+#include "x86/vfilter.h"
 
 #if X264_BIT_DEPTH > 8
 typedef uint16_t pixel;
@@ -70,18 +71,6 @@ static obe_cli_csp_t obe_cli_csps[] =
     [PIX_FMT_YUV420P16] = { 3, { 1, .5, .5 }, { 1, .5, .5 }, 2, 2, 16 },
 };
 
-static uint32_t convert_cpu_to_flag( uint32_t avutil_cpu )
-{
-    uint32_t swscale_cpu = 0;
-    if( avutil_cpu & AV_CPU_FLAG_ALTIVEC )
-        swscale_cpu |= SWS_CPU_CAPS_ALTIVEC;
-    if( avutil_cpu & AV_CPU_FLAG_MMX )
-        swscale_cpu |= SWS_CPU_CAPS_MMX;
-    if( avutil_cpu & AV_CPU_FLAG_MMX2 )
-        swscale_cpu |= SWS_CPU_CAPS_MMX2;
-    return swscale_cpu;
-}
-
 static void scale_plane( uint16_t *src, int stride, int width, int height, int lshift, int rshift )
 {
     for( int i = 0; i < height; i++ )
@@ -96,6 +85,8 @@ static void scale_plane( uint16_t *src, int stride, int width, int height, int l
 static void init_filter( obe_vid_filter_ctx_t *vfilt )
 {
     vfilt->avutil_cpu = av_get_cpu_flags();
+
+#if 0
     vfilt->scale_plane = scale_plane;
 
     if( vfilt->avutil_cpu & AV_CPU_FLAG_MMX2 )
@@ -106,6 +97,7 @@ static void init_filter( obe_vid_filter_ctx_t *vfilt )
 
     if( vfilt->avutil_cpu & AV_CPU_FLAG_AVX )
         vfilt->scale_plane = obe_scale_plane_avx;
+#endif
 }
 
 static int scale_frame( obe_vid_filter_ctx_t *vfilt, obe_raw_frame_t *raw_frame )
@@ -130,7 +122,7 @@ static int scale_frame( obe_vid_filter_ctx_t *vfilt, obe_raw_frame_t *raw_frame 
         vfilt->scale_plane( src, img->stride[i], width, height, lshift, rshift );
     }
 
-    img->csp = img->csp == PIX_FMT_YUV420P10 ? PIX_FMT_YUV420P16 : PIX_FMT_YUV422P16;
+    img->csp = img->csp == PIX_FMT_YUV420P ? PIX_FMT_YUV420P10 : PIX_FMT_YUV422P10;
 
     return 0;
 }
@@ -141,10 +133,8 @@ static int downconvert_frame( obe_vid_filter_ctx_t *vfilt, obe_raw_frame_t *raw_
 
     if( !vfilt->sws_ctx )
     {
-        vfilt->sws_ctx_flags |= SWS_SPLINE;
-        vfilt->sws_ctx_flags |= SWS_FULL_CHR_H_INT | SWS_FULL_CHR_H_INP | SWS_ACCURATE_RND;
-        vfilt->sws_ctx_flags |= convert_cpu_to_flag( vfilt->avutil_cpu );
-        vfilt->dst_pix_fmt = raw_frame->img.csp == PIX_FMT_YUV422P ? PIX_FMT_YUV420P : PIX_FMT_YUV420P16;
+        vfilt->sws_ctx_flags |= SWS_FULL_CHR_H_INT | SWS_FULL_CHR_H_INP | SWS_ACCURATE_RND | SWS_SPLINE;
+        vfilt->dst_pix_fmt = raw_frame->img.csp == PIX_FMT_YUV422P10 ? PIX_FMT_YUV420P10 : PIX_FMT_YUV420P;
 
         vfilt->sws_ctx = sws_getContext( raw_frame->img.width, raw_frame->img.height, raw_frame->img.csp,
                                          raw_frame->img.width, raw_frame->img.height, vfilt->dst_pix_fmt,
@@ -182,6 +172,8 @@ static int csp_num_interleaved( int csp, int plane )
 {
     return ( csp == PIX_FMT_NV12 && plane == 1 ) ? 2 : 1;
 }
+
+#if 0
 
 /* The dithering algorithm is based on Sierra-2-4A error diffusion. It has been
  * written in such a way so that if the source has been upconverted using the
@@ -255,6 +247,68 @@ static int dither_image( obe_raw_frame_t *raw_frame, int16_t *error_buf )
     return 0;
 }
 
+#endif
+
+static int dither_image( obe_vid_filter_ctx_t *vfilt, obe_raw_frame_t *raw_frame )
+{
+    obe_image_t *img = &raw_frame->img;
+    obe_image_t tmp_image = {0};
+    obe_image_t *out = &tmp_image;
+
+    tmp_image.csp = PIX_FMT_YUV420P;
+    tmp_image.width = raw_frame->img.width;
+    tmp_image.height = raw_frame->img.height;
+
+    if( av_image_alloc( tmp_image.plane, tmp_image.stride, tmp_image.width, tmp_image.height,
+                        tmp_image.csp, 16 ) < 0 )
+    {
+        syslog( LOG_ERR, "Malloc failed\n" );
+        return -1;
+    }
+
+    for( int i = 0; i < img->planes; i++ )
+    {
+        const int src_depth = av_pix_fmt_descriptors[img->csp].comp[i].depth_minus1+1;
+        const int dst_depth = av_pix_fmt_descriptors[out->csp].comp[i].depth_minus1+1;
+
+        uint16_t scale = obe_dither_scale[dst_depth-1][src_depth-1];
+        int shift = src_depth-dst_depth + obe_dither_scale[src_depth-2][dst_depth-1];
+
+        int num_interleaved = csp_num_interleaved( img->csp, i );
+        int height = obe_cli_csps[img->csp].height[i] * img->height;
+        int width = obe_cli_csps[img->csp].width[i] * img->width / num_interleaved;
+        uint16_t *src = (uint16_t*)img->plane[i];
+        uint8_t *dst = out->plane[i];
+
+        for( int j = 0; j < height; j++ )
+        {
+            const uint8_t *dither = obe_dithers[j&7];
+            int k;
+            for (k = 0; k < width-7; k+=8)
+            {
+                dst[k+0] = (src[k+0] + dither[0])*scale>>shift;
+                dst[k+1] = (src[k+1] + dither[1])*scale>>shift;
+                dst[k+2] = (src[k+2] + dither[2])*scale>>shift;
+                dst[k+3] = (src[k+3] + dither[3])*scale>>shift;
+                dst[k+4] = (src[k+4] + dither[4])*scale>>shift;
+                dst[k+5] = (src[k+5] + dither[5])*scale>>shift;
+                dst[k+6] = (src[k+6] + dither[6])*scale>>shift;
+                dst[k+7] = (src[k+7] + dither[7])*scale>>shift;
+            }
+            for (; k < width; k++)\
+                dst[k] = (src[k] + dither[k&7])*scale>>shift;
+
+            src += img->stride[i] / 2;
+            dst += out->stride[i];
+	}
+    }
+
+    raw_frame->release_data( raw_frame );
+    memcpy( &raw_frame->img, out, sizeof(obe_image_t) );
+
+    return 0;
+}
+
 int encapsulate_user_data( obe_raw_frame_t *raw_frame, obe_int_input_stream_t *input_stream )
 {
     /* Encapsulate user-data */
@@ -311,31 +365,15 @@ void *start_filter( void *ptr )
 
         /* TODO: scale 8-bit to 16-bit */
 
-        if( raw_frame->img.csp == PIX_FMT_YUV420P10 || raw_frame->img.csp == PIX_FMT_YUV422P10 )
-        {
-            if( scale_frame( vfilt, raw_frame ) < 0 )
-                goto end;
-        }
-
-        if( raw_frame->img.csp == PIX_FMT_YUV422P || raw_frame->img.csp == PIX_FMT_YUV422P16 )
+        if( raw_frame->img.csp == PIX_FMT_YUV422P || raw_frame->img.csp == PIX_FMT_YUV422P10 )
         {
             if( downconvert_frame( vfilt, raw_frame ) < 0 )
                 goto end;
         }
 
-        if( raw_frame->img.csp == PIX_FMT_YUV420P16 )
+        if( raw_frame->img.csp == PIX_FMT_YUV420P10 && X264_BIT_DEPTH == 8 )
         {
-            if( !vfilt->error_buf )
-            {
-                vfilt->error_buf = malloc( (raw_frame->img.width+1) * sizeof(*vfilt->error_buf) );
-                if( !vfilt->error_buf )
-                {
-                    fprintf( stderr, "Malloc failed\n" );
-                    goto end;
-                }
-            }
-
-            if( dither_image( raw_frame, vfilt->error_buf ) < 0 )
+            if( dither_image( vfilt, raw_frame ) < 0 )
                 goto end;
         }
 
@@ -351,9 +389,6 @@ end:
     {
         if( vfilt->sws_ctx )
             sws_freeContext( vfilt->sws_ctx );
-
-        if( vfilt->error_buf )
-            free( vfilt->error_buf );
 
         free( vfilt );
     }
