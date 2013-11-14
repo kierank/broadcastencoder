71044b5851acd3ad88dbbed9e12fbc9a60871f82
Kieran Kunhya <kierank@ob-encoder.com>
2013-11-14 21:41:23 +0000

    Correct RTP packet length

--- output/ip/ip.c
+++ output/ip/ip.c
@@ -252,7 +252,7 @@ static int write_rtp_pkt( hnd_t handle, uint8_t *data, int len, int64_t timestam
         *column_ts ^= ts_90;
         *row_ts ^= ts_90;
 
-        xor_packet_c( &column[RTP_HEADER_SIZE+FEC_HEADER_SIZE], p_rtp->pkt, TS_PACKETS_SIZE );
+        xor_packet_c( &column[RTP_HEADER_SIZE+FEC_HEADER_SIZE], p_rtp->pkt, RTP_PACKET_SIZE );
 
         /* Check if we can send packets. Start with rows to match other encoders
          * Write the headers in advance */
