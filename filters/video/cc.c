3609678f90d64812f0a7cd59f8140f9dbe2ba968
Kieran Kunhya <kierank@obe.tv>
2014-07-23 16:53:31 +0100

    Support MPEG-2 closed captions

--- filters/video/cc.c
+++ filters/video/cc.c
@@ -97,6 +97,7 @@ int write_608_cc( obe_user_data_t *user_data, obe_raw_frame_t *raw_frame )
     const int data_type_code    = 0x03;
     int cc_count                = 0;
     const int echostar_captions = 0;
+    const int mpeg4 = 0;
 
     /* TODO: when MPEG-2 is added make this do the right thing */
     /* FIXME: enable echostar captions and add more types */
@@ -115,8 +116,11 @@ int write_608_cc( obe_user_data_t *user_data, obe_raw_frame_t *raw_frame )
 
     bs_init( &r, temp, 1000 );
 
-    /* N.B MPEG-4 only */
-    write_itu_t_codes( &r );
+    if( mpeg4 )
+    {
+        /* N.B MPEG-4 only */
+        write_itu_t_codes( &r );
+    }
 
     if( !echostar_captions )
     {
@@ -188,14 +192,18 @@ static int write_708_cc( obe_user_data_t *user_data, uint8_t *start, int cc_coun
     uint8_t temp[1000];
     const char *user_identifier = "GA94";
     const int data_type_code    = 0x03;
+    const int mpeg4 = 0;
 
     /* TODO: when MPEG-2 is added make this do the right thing */
     /* FIXME: enable echostar captions and add more types */
 
     bs_init( &s, temp, 1000 );
 
-    /* N.B MPEG-4 only */
-    write_itu_t_codes( &s );
+    if( mpeg4 )
+    {
+        /* N.B MPEG-4 only */
+        write_itu_t_codes( &r );
+    }
 
     for( int i = 0; i < 4; i++ )
         bs_write( &s, 8, user_identifier[i] ); // user_identifier
