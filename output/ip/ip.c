070d38a4164fc2751365c5eac1d26090ddd70733
Rafaël Carré <funman@videolan.org>
2018-04-30 15:47:24 +0200

    ip: write_fec_header already takes an int16, no need to AND

--- output/ip/ip.c
+++ output/ip/ip.c
@@ -319,7 +319,7 @@ static int fec(obe_rtp_ctx *p_rtp, int fec_type, uint8_t *pkt_ptr, uint32_t ts_9
     if( column_idx == (p_rtp->fec_columns-1) )
     {
         write_rtp_header( row, FEC_PAYLOAD_TYPE, p_rtp->row_seq++, 0, 0 );
-        write_fec_header( p_rtp, row, 1, (p_rtp->seq - column_idx) & 0xffff );
+        write_fec_header( p_rtp, row, 1, p_rtp->seq - column_idx );
 
         if( write_fec_packet( p_rtp->row_handle, row ) < 0 )
             ret = -1;
@@ -330,7 +330,7 @@ static int fec(obe_rtp_ctx *p_rtp, int fec_type, uint8_t *pkt_ptr, uint32_t ts_9
         if( row_idx == (p_rtp->fec_rows-1) )
         {
             write_rtp_header( column, FEC_PAYLOAD_TYPE, p_rtp->column_seq++, 0, 0 );
-            write_fec_header( p_rtp, column, 0, (p_rtp->seq - (p_rtp->fec_columns*(p_rtp->fec_rows-1))) & 0xffff );
+            write_fec_header( p_rtp, column, 0, p_rtp->seq - (p_rtp->fec_columns*(p_rtp->fec_rows-1)) );
         }
 
         /* Interleave the column FEC data from the previous matrix */
@@ -349,7 +349,7 @@ static int fec(obe_rtp_ctx *p_rtp, int fec_type, uint8_t *pkt_ptr, uint32_t ts_9
             if( deoffsetted_seq % ( p_rtp->fec_columns * p_rtp->fec_rows ) == 0 )
             {
                 write_rtp_header( column, FEC_PAYLOAD_TYPE, p_rtp->column_seq++, 0, 0 );
-                write_fec_header( p_rtp, column, 0, (p_rtp->seq - (p_rtp->fec_columns*p_rtp->fec_rows)) & 0xffff );
+                write_fec_header( p_rtp, column, 0, p_rtp->seq - (p_rtp->fec_columns*p_rtp->fec_rows) );
 
                 if( write_fec_packet( p_rtp->column_handle, column ) < 0 )
                     ret = -1;
