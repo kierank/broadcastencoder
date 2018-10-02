367f6192ee37995bb34d5392fd52243fa3955206
Rafaël Carré <funman@videolan.org>
2018-10-02 11:27:13 +0200

    Fix numa mask processing
    Remove trailing spaces and get the whole mask

--- obed.sh
+++ obed.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-mask=`numactl -s | grep ^nodebind -|cut -d\  -f2|rev|cut -d\  -f1`
+mask=`numactl -s | grep ^nodebind -|cut -d\  -f2-|sed 's/ *$//g'|rev|cut -d\  -f1`
 i=`echo $(($1 & $mask))`
 exec numactl -m $i -N $i "`dirname $0`"/obed $1
